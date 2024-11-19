890db62af6c3b4ab75942c2a6e12de54974d8261
tim0618 <tim061801@gmail.com>
2024-11-20 01:00:33 +0800

    feat:source can emit 'account'、'num'

--- src/components/ManageAllData.vue
+++ src/components/ManageAllData.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="manageAllData">
-    <div class="q-pa-md" v-for="n in 4" :key="n">
-      <q-card class="my-card">
-        <q-video
-          src="https://www.youtube.com/embed/mr5W8MqW0Mg?si=x_1VXyL3O9YQDFTY"
-        />
-        <q-card-section>
-          <div class="text-h6">First</div>
-          <div class="text-subtitle2">by Chun Ting</div>
-        </q-card-section>
-      </q-card>
+    <div class="q-pa-md">
+      <div class="my-card" @click="source(1)"></div>
     </div>
   </div>
 </template>
+
+<script setup>
+const emit = defineEmits(["sourceCount"]);
+
+const source = (num) => {
+  const account = localStorage.getItem("account");
+  emit("sourceCount", account, num);
+
+  // console.log(account + "課程" + num + "觀看+1");
+};
+</script>
+
 <style scoped>
 .manageAllData {
   display: flex;
@@ -20,8 +24,10 @@
   flex-wrap: wrap;
 }
 .my-card {
-  width: 100%;
-  max-width: 250px;
+  width: 300px;
+  height: 300px;
+  border: 1px solid black;
+  background-color: cornflowerblue;
 }
 
 /* @media (max-width: 960px) {
@@ -31,3 +37,13 @@
   }
 } */
 </style>
+
+<!-- <q-card class="my-card">
+        <q-video
+          src="https://www.youtube.com/embed/mr5W8MqW0Mg?si=x_1VXyL3O9YQDFTY"
+        />
+        <q-card-section>
+          <div class="text-h6">First</div>
+          <div class="text-subtitle2">by Chun Ting</div>
+        </q-card-section>
+      </q-card> -->
--- src/page/LearnData.vue
+++ src/page/LearnData.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <manage-all-data />
+    <manage-all-data @sourceCount="accountSourceCount" />
   </div>
 
   <div style="border: 1px black solid">
@@ -28,6 +28,11 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import ManageAllData from "../components/ManageAllData.vue";
+
+const accountSourceCount = (account, num) => {
+  console.log("account", account);
+  console.log("num", num);
+};
 
 const columns = [
   {
@@ -59,53 +64,53 @@ const columns = [
 
 const seed = [
   {
-    name: "Frozen Yogurt",
-    calories: 159,
-    one: 6.0,
-    two: 24,
-    three: 4.0,
-    four: 87,
-  },
-  {
-    name: "Ice cream sandwich",
-    calories: 237,
-    one: 9.0,
-    two: 37,
-    three: 4.3,
-    four: 129,
-  },
-  {
-    name: "Eclair",
-    calories: 262,
-    one: 16.0,
-    two: 23,
-    three: 6.0,
-    four: 337,
-  },
-  {
-    name: "Cupcake",
-    calories: 305,
-    one: 3.7,
-    two: 67,
-    three: 4.3,
-    four: 413,
-  },
-  {
-    name: "Gingerbread",
-    calories: 356,
-    one: 16.0,
-    two: 49,
-    three: 3.9,
-    four: 327,
-  },
-  {
-    name: "Jelly bean",
+    name: "Timbro",
     calories: 0,
     one: 0,
     two: 0,
     three: 0,
     four: 0,
   },
+  // {
+  //   name: "Ice cream sandwich",
+  //   calories: 237,
+  //   one: 9.0,
+  //   two: 37,
+  //   three: 4.3,
+  //   four: 129,
+  // },
+  // {
+  //   name: "Eclair",
+  //   calories: 262,
+  //   one: 16.0,
+  //   two: 23,
+  //   three: 6.0,
+  //   four: 337,
+  // },
+  // {
+  //   name: "Cupcake",
+  //   calories: 305,
+  //   one: 3.7,
+  //   two: 67,
+  //   three: 4.3,
+  //   four: 413,
+  // },
+  // {
+  //   name: "Gingerbread",
+  //   calories: 356,
+  //   one: 16.0,
+  //   two: 49,
+  //   three: 3.9,
+  //   four: 327,
+  // },
+  // {
+  //   name: "Jelly bean",
+  //   calories: 0,
+  //   one: 0,
+  //   two: 0,
+  //   three: 0,
+  //   four: 0,
+  // },
   // {
   //   name: "Lollipop",
   //   calories: 392,
